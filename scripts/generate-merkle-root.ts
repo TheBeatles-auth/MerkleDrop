6ad6c9b2ae9f2b4a8dcb375b54aa62769ed277f1
Jill04 <69854931+Jill04@users.noreply.github.com>
2021-02-10 16:36:55 +0530

    Update generate-merkle-root.ts

--- scripts/generate-merkle-root.ts
+++ scripts/generate-merkle-root.ts
@@ -1,17 +1,13 @@
-const program  = require ('commander');
+const program = require('commander'); 
 const  fs = require('fs');
 import { parseBalanceMap } from '../src/parse-balance-map'
-
+const data = "scripts/Address.json";
 program
   .version('0.0.0')
-  .requiredOption(
-    '-i, --input <path>',
-    'input JSON file location containing a map of account addresses to string balances'
-  )
-
+ 
 program.parse(process.argv)
 
-const json = JSON.parse(fs.readFileSync(program.input, { encoding: 'utf8' }))
+const json = JSON.parse(fs.readFileSync(data, { encoding: 'utf8' }))
 
 if (typeof json !== 'object') throw new Error('Invalid JSON')
 
