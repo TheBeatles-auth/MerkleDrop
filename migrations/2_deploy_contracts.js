f18da1c61afb87089f4ee376f2a8a0938fac5fc2
Jill04 <69854931+Jill04@users.noreply.github.com>
2021-02-19 11:46:18 +0530

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
 var MerkleDrop = artifacts.require("MerkleDrop.sol");
 var TokenBasic = artifacts.require("TokenBasic.sol");
 var TokenBasic1 = artifacts.require("TokenBasic.sol");
+var Lock = artifacts.require("Lock.sol");
 
 module.exports = async function(deployer)
 {
 	 await deployer.deploy(TokenBasic);
 	 await deployer.deploy(TokenBasic1);
-	 await deployer.deploy(MerkleDrop);
+	 await deployer.deploy(Lock);
+	 await deployer.deploy(MerkleDrop,Lock.address,web3.utils.toWei("0.05","ether"),web3.utils.toWei("0.05","ether"),'0x31de0c08f72fe94aadfa9adbfabb5b23238b9ce1');
 };	
